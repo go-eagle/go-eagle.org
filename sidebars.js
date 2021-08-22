57ea7515d32f6120bd218173e2ccb3cd0af8ac3f
easygogo <quanlongwang@gmail.com>
2021-08-22 23:08:13 +0800

    init docs

---
+++ sidebars.js
@@ -0,0 +1,32 @@
+/**
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for each doc of that group
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
+ */
+
+module.exports = {
+  /*
+  tutorialSidebar: [
+    {
+      type: 'category',
+      label: 'Tutorial',
+      items: ['hello'],
+    },
+  ],
+   */
+  docs: [
+    {
+      type: 'category',
+      label: '快速开始',
+      collapsed: false,
+      items: [
+        'intro',
+      ],
+    },
+  ],
+};
