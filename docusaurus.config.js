83a691722a4ebf63f371d0069ad830a7500695c6
qloog <quanlongwang@gmail.com>
2021-09-10 23:07:27 +0800

    add hideableSidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,6 +14,7 @@ module.exports = {
   projectName: 'go-eagle.org', // Usually your repo name.
   trailingSlash: false,
   themeConfig: {
+    hideableSidebar: true,
     navbar: {
       title: 'Eagle',
       logo: {
